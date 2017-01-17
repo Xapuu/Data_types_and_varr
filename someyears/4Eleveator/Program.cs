7e7db4fc564e7149a74fde93ee23dd07abc3f546
Xapuu <xapu.ivanov@gmail.com>
2017-01-17 20:08:53 +0200

    2 excercies Softuni

---
+++ someyears/4Eleveator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Eleveator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int person = int.Parse(Console.ReadLine());
+            int cap = int.Parse(Console.ReadLine());
+            double course = Math.Ceiling((double)person / cap);
+
+            Console.WriteLine(course);
+
+
+        }
+    }
+}
