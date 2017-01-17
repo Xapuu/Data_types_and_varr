e094a31c10d7631d556437c617c5088bcefded0a
Xapuu <xapu.ivanov@gmail.com>
2017-01-17 19:43:05 +0200

    second exc from data base Softuni

---
+++ someyears/CircleArea12digits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea12digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            double answer = Math.PI * r * r;
+
+            Console.WriteLine("{0:F12}",answer);
+        }
+    }
+}
