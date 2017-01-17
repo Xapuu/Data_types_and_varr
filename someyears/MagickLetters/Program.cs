31567f7e25fece3d93dde1d1e357a4c307bc8c95
Xapuu <xapu.ivanov@gmail.com>
2017-01-17 21:02:16 +0200

    some files

---
+++ someyears/MagickLetters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int letter = int.Parse(Console.ReadLine())+97;
+            for (int x = 97; x < letter; x++)
+            {
+                
+                for (int z = 97; z < letter; z++)
+                {
+                    
+                    for (int i = 97; i < letter; i++)
+                    {
+                        Console.WriteLine((char)x+""+(char)z+""+(char)i);
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
